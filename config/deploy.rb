ea1b9446ff5bb1e0b0d112226eaddd300d13dc47
Guilherme Taschetto <gtaschetto@gmail.com>
2015-07-08 11:29:25 -0300

    Add Mina script for deploying Hubot.

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require 'mina/git'
+
+set :domain, '104.236.246.109'
+set :port, '22'
+
+set :path, '$HOME/bin'
+set :deploy_to, '/home/hubot/app'
+set :repository, 'git@github.com:fator7/hubot.git'
+set :branch, 'master'
+
+set :user, 'hubot'
+set :forward_agent, true
+
+task :environment do
+  queue! %[#{echo_cmd %{export PATH="#{path}:$PATH"}}]
+end
+
+task :setup => :environment do
+  queue! %[#{echo_cmd %{ssh-keyscan -H github.com > ~/.ssh/known_hosts}}]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+  end
+end
